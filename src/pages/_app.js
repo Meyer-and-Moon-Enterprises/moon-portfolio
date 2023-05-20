dde5d4b2181a6dde53f76191c987072d398fe4f7
ianm1873 <ian@iwm.dev>
2023-05-19 22:12:31 -0700

    remove G analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -46,7 +46,7 @@ export default function App({ Component, pageProps }) {
           <title>Genuine Jesse</title>
         </Head>
 
-        <GoogleAnalytics measurementId='G-SZES6HZZJM'/>
+        {/* <GoogleAnalytics measurementId='G-SZES6HZZJM'/> */}
 
         <Navbar />
         <AnimatePresence mode="wait" initial={false}>
@@ -57,7 +57,7 @@ export default function App({ Component, pageProps }) {
         <Footer />  
 
       </PostHogProvider>
-      {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossOrigin="anonymous"></script> */}
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossOrigin="anonymous"></script>
 
 
 
